be060cc31979a6f94757c06a266c67e5a976359c
juanf.gallo <jufegare@gmail.com>
2019-04-23 15:52:32 -0500

    First commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 
 import Shelf from '../Shelf';
-import Filter from '../Shelf/Filter';
-import GithubCorner from '../github/Corner';
+
 import FloatCart from '../FloatCart';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <GithubCorner />
         <main>
-          <Filter />
           <Shelf />
         </main>
         <FloatCart />
--- src/components/Shelf/ProductList/Product/index.js
+++ src/components/Shelf/ProductList/Product/index.js
@@ -33,12 +33,10 @@ const Product = ({ product, addProduct }) => {
       onClick={() => addProduct(product)}
       data-sku={product.sku}
     >
-      {product.isFreeShipping && (
-        <div className="shelf-stopper">Free shipping</div>
-      )}
+      
       <Thumb
         classes="shelf-item__thumb"
-        src={require(`../../../../static/products/${product.sku}_1.jpg`)}
+       
         alt={product.title}
       />
       <p className="shelf-item__title">{product.title}</p>
@@ -48,7 +46,6 @@ const Product = ({ product, addProduct }) => {
           <b>{formattedPrice.substr(0, formattedPrice.length - 3)}</b>
           <span>{formattedPrice.substr(formattedPrice.length - 3, 3)}</span>
         </div>
-        {productInstallment}
       </div>
       <div className="shelf-item__buy-btn">Add to cart</div>
     </div>
